2856c10cb5bb6a1092e90c23751c5230ada5138f
Cláudio Rapôso <engcfraposo@gmail.com>
2022-03-17 14:38:51 -0300

    Category Resource

---
+++ src/main/java/com/cloudfox/cfcatalog/resources/CategoryResource.java
@@ -0,0 +1,24 @@
+package com.cloudfox.cfcatalog.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cloudfox.cfcatalog.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Books"));
+		list.add(new Category(1L, "Electronics"));
+		return ResponseEntity.ok().body(list);
+	}
+}
