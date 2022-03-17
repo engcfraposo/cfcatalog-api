deeef155ee307ecc2d037b6c7baaf3452352fe97
Cláudio Rapôso <engcfraposo@gmail.com>
2022-03-17 13:50:23 -0300

    initial commit

---
+++ src/main/java/com/cloudfox/cfcatalog/CfcatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.cloudfox.cfcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfcatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfcatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudfox/cfcatalog/CfcatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudfox.cfcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfcatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
