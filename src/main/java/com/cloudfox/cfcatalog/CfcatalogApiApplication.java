d33b0359fe15c5d84c0dccb189bfe95ace70ff6a
Cláudio Rapôso <engcfraposo@gmail.com>
2022-03-23 20:53:37 -0300

    Update CfcatalogApiApplication.java

--- src/main/java/com/cloudfox/cfcatalog/CfcatalogApiApplication.java
+++ src/main/java/com/cloudfox/cfcatalog/CfcatalogApiApplication.java
@@ -3,26 +3,11 @@ package com.cloudfox.cfcatalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.cloudfox.cfcatalog.resolvers.CategoryMutation;
-import com.cloudfox.cfcatalog.resolvers.CategoryQuery;
-import com.coxautodev.graphql.tools.SchemaParser;
-
-import graphql.GraphQL;
-import graphql.schema.GraphQLSchema;
-
 @SpringBootApplication
 public class CfcatalogApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfcatalogApiApplication.class, args);
-		GraphQLSchema graphQLSchema =
-		        SchemaParser.newParser()
-		            .file("swapi.graphqls")
-		            .resolvers(new CategoryQuery(), new CategoryMutation())
-		            .build()
-		            .makeExecutableSchema();
-		GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
-		graphQL.execute("{ droid(id: \"2001\") { name } } ");
 	}
 
 }
