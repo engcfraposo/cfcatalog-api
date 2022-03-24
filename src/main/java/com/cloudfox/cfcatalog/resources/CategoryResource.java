fefb31d884dd570ca4e42c0c24abd1d4e8277ef3
Cláudio Rapôso <engcfraposo@gmail.com>
2022-03-23 21:05:40 -0300

    refactor Pageable

--- src/main/java/com/cloudfox/cfcatalog/resources/CategoryResource.java
+++ src/main/java/com/cloudfox/cfcatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -29,14 +29,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<CategoryDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-			@RequestParam(value = "direction", defaultValue = "DESC") String direction	
-		){
-			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		Page<CategoryDTO> listDTO = service.findAllPaged(pageRequest);	
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable){
+		Page<CategoryDTO> listDTO = service.findAllPaged(pageable);	
 		return ResponseEntity.ok().body(listDTO);
 	}
 	
--- src/main/java/com/cloudfox/cfcatalog/resources/ProductResource.java
+++ src/main/java/com/cloudfox/cfcatalog/resources/ProductResource.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,14 +30,8 @@ public class ProductResource {
 	private ProductService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-			@RequestParam(value = "direction", defaultValue = "DESC") String direction	
-		){
-			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		Page<ProductDTO> listDTO = service.findAllPaged(pageRequest);	
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+		Page<ProductDTO> listDTO = service.findAllPaged(pageable);	
 		return ResponseEntity.ok().body(listDTO);
 	}
 	
